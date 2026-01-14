d3fc0c6ca190471df9d07ddf32f3c772456cb32d
chanintorndti <chanintorn.dti@gmail.com>
2026-01-14 12:19:37 +0700

    Commit 01: ขึ้นโปรเจ็กต์ หน้าจอ HomeUi เสร็จแล้ว ที่เหลือยังไม่ได้ทำ

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iot_second_app/views/home_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main(){
+  runApp(
+    //เรียกใช้งานคลาสที่เรียกใช้ Widget หลักของแอปฯ (MaterialApp)
+    FlutterIoTSecondApp(),
+  );
+}
+//-----------------------------------------
+class FlutterIoTSecondApp extends StatefulWidget {
+  const FlutterIoTSecondApp({super.key});
+
+  @override
+  State<FlutterIoTSecondApp> createState() => _FlutterIoTSecondAppState();
+}
+
+class _FlutterIoTSecondAppState extends State<FlutterIoTSecondApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //ซ่อนป้าย debug คาดสีแดง
+      debugShowCheckedModeBanner: false,
+      //กำหนดหน้าจอแรก
+      home: HomeUi(),
+      //กำหนด Theme โดยเฉพาะ font หลักของแอปฯ
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+    );
+  }
+}
